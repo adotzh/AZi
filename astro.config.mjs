0f7a5b1ba3a86715d13cacdba48913fc4cad09f9
Anastasiya Zhiboedova <39925939+adotzh@users.noreply.github.com>
2026-05-19 23:41:36 -0700

    fix: remove base path now that repo is deployed at root

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://adotzh.github.io',
-  base: '/AZi',
   output: 'static',
   trailingSlash: 'never'
 });
